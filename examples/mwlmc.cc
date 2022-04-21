51a752f406f72ad304c4502d5c6f9dcd4a58d8ca
Sophia Lilleengen <s.lilleengen@surrey.ac.uk>
2022-04-21 17:30:42 -0400

    starting to wrap; builds but error when importing in python

--- examples/mwlmc.cc
+++ examples/mwlmc.cc
@@ -4,7 +4,7 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
-
+#include "fullintegrate.h"
 
 namespace py = pybind11;
 
@@ -13,7 +13,17 @@ int add(int i, int j) {
 }
 
 PYBIND11_MODULE(mwlmc, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+//     m.doc() = "pybind11 example plugin"; // optional module docstring
 
-    m.def("add", &add, "A function that adds two numbers");
+//     m.def("add", &add, "A function that adds two numbers");
+
+    py::class_<MWLMC>(m, "MWLMC")
+        .def(py::init<>())
+        .def("mwhalo_fields", &MWLMC::mwhalo_fields)
+        .def("lmc_fields", &MWLMC::lmc_fields)
+        .def("mwd_fields", &MWLMC::mwd_fields)
+        .def("all_forces", &MWLMC::all_forces)
+        .def("orbit", &MWLMC::orbit)
+        .def("print_orbit", &MWLMC::print_orbit);
+
 }
